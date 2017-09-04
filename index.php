e0f3e4533ae2b423e9a9d1d5dab6c1734520534e
Julien Gargot <julien@g-u-i.me>
2017-09-04 12:50:06 +0200

    First test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Lighthouse</title>
+  <!-- <link rel="stylesheet" type="text/css" href="dist/css/pantoufle.min.css"> -->
+  <link rel="stylesheet" type="text/css" href="assets/css/pantoufle.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/theme.css">
+</head>
+<body>
+
+  <!-- start page -->
+  <section class="page">
+    <div class="content">
+
+      <!-- Add your content here -->
+      <?php include 'content/dream-1.php' ?>
+
+    </div>
+  </section>
+  <!-- end page -->
+
+  <!-- <script type="text/javascript" src="dist/js/pantoufle.min.js"></script> -->
+  <script type="text/javascript" src="assets/js/pantoufle.js"></script>
+</body>
+</html>
